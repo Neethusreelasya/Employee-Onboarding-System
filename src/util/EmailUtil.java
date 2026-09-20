cf9d56d8bfffcce590b4bb5c1a57de89e2c9264e
Neethusreelasya <neethuarepalli@gmail.com>
2026-09-20 18:20:40 +0530

    Switch email sending to SendGrid HTTP API for Render compatibility

--- src/util/EmailUtil.java
+++ src/util/EmailUtil.java
@@ -1,41 +1,46 @@
 package util;
 
-import jakarta.mail.*;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-
-import java.util.Properties;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class EmailUtil {
 
-    private static final String FROM_EMAIL = "arepallineethusreelasya@gmail.com";
-    private static final String APP_PASSWORD = "osgpsuzdmqyjujkv";
+    private static final String API_KEY = System.getenv("SENDGRID_API_KEY") != null
+            ? System.getenv("SENDGRID_API_KEY") : "your_sendgrid_api_key_here";
+    private static final String FROM_EMAIL = System.getenv("MAIL_FROM") != null
+            ? System.getenv("MAIL_FROM") : "your_verified_sender_email_here";
 
     public static void sendEmail(String toEmail, String subject, String body) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-
-        Session session = Session.getInstance(props, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(FROM_EMAIL, APP_PASSWORD);
-            }
-        });
-
         try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(FROM_EMAIL));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
-            message.setSubject(subject);
-            message.setText(body);
+            String jsonBody = "{"
+                    + "\"personalizations\":[{\"to\":[{\"email\":\"" + toEmail + "\"}]}],"
+                    + "\"from\":{\"email\":\"" + FROM_EMAIL + "\"},"
+                    + "\"subject\":\"" + escapeJson(subject) + "\","
+                    + "\"content\":[{\"type\":\"text/plain\",\"value\":\"" + escapeJson(body) + "\"}]"
+                    + "}";
 
-            Transport.send(message);
-            System.out.println("Email sent to " + toEmail);
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.sendgrid.com/v3/mail/send"))
+                    .header("Authorization", "Bearer " + API_KEY)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                    .build();
 
-        } catch (MessagingException e) {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("SendGrid API response: " + response.statusCode() + " " + response.body());
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static String escapeJson(String text) {
+        return text.replace("\\", "\\\\")
+                    .replace("\"", "\\\"")
+                    .replace("\n", "\\n")
+                    .replace("\r", "");
     }
 }
